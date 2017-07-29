2ca6a0cf5d57b4b3bb58151b9d3764a2f7d1f331
blackdante <kamil.m.kielbasa@gmail.com>
2017-07-29 12:27:31 +0200

    Add docker builder

---
+++ src/Fibon.Service/ICalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Fibon.Service
+{
+    public interface ICalculator
+    {
+        int Fib(int n);
+    }
+
+    public class Fast : ICalculator
+    {
+        public int Fib(int n)
+        {
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < n; i++) 
+            {
+                int temp = a;
+                a = b;
+                b = b + temp;
+            }
+
+            return a;
+        }
+    }
+}
---
+++ tests/Fibon.Tests/FibTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using Fibon.Service;
+
+namespace Fibon.Tests
+{
+    public class FibTests
+    {
+		[Theory]
+        [InlineData(0,0)]
+        [InlineData(1,1)]
+        [InlineData(2,1)]
+        [InlineData(7,13)]
+		public void Fib_RetursCorrentValues(int number, int expected)
+        {
+			ICalculator calc = new Fast();
+			int result = calc.Fib(number);
+
+			Assert.Equal(expected, result);
+        }
+    }
+}
